55ed3d2a8040e68ecef8521910ce1af34a499f9e
Janne Kauppinen <janne.kauppinen4321@gmail.com>
2024-08-22 14:31:41 +0300

    Create a simple working application.

--- smoke/src/main.rs
+++ smoke/src/main.rs
@@ -1,3 +1,24 @@
+// use dioxus_desktop::{Config, WindowBuilder, launch_with_props};
+use dioxus::prelude::*;
+use dioxus::events::*;
+use dioxus_desktop::{Config, WindowBuilder, LogicalSize};
+
+pub fn main_page() -> Element {
+    rsx! {"jeejee"}
+}
+
 fn main() {
-    println!("Smoke!");
+
+    // Main application configuration.
+    let config = Config::new().with_window(
+        WindowBuilder::default()
+        .with_title("Smoke!")
+        .with_inner_size(LogicalSize::new(1280.0, 1024.0)),
+        );
+
+
+    // Launch the main application.
+    LaunchBuilder::new()
+        .with_cfg(desktop!(config))
+        .launch(main_page);
 }
